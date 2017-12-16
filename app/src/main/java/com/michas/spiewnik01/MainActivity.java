e4246f076aa39091c4df74d473dc731eeda124e4
mstepniak <michas1991@gmail.com>
2017-12-16 20:36:11 +0100

    change mainActivity to singleTask

--- app/src/main/java/com/michas/spiewnik01/MainActivity.java
+++ app/src/main/java/com/michas/spiewnik01/MainActivity.java
@@ -1,12 +1,9 @@
 package com.michas.spiewnik01;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
